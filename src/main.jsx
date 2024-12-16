e936263eaef432685f7697708b6c64dc176dfbc7
dianaboikoo <eaa23dibo@students.eaaa.dk>
2024-12-16 19:32:45 +0100

    router

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,13 @@
-// index.js or main.jsx
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import App from "./App";
+import App from "./App.jsx";
+import "./styles.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>
+    <React.StrictMode>
+        <BrowserRouter basename={import.meta.env.DEV ? "/" : "/gogreen/"}>
+            <App />
+        </BrowserRouter>
+    </React.StrictMode>
 );
